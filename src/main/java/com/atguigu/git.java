94f181511f8a2f64e906d989a1b806a558cc1ebf
txg0612 <565826038@qq.com>
2024-01-08 14:20:15 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -6,6 +6,7 @@ public class git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test!");
 
     }
 }
